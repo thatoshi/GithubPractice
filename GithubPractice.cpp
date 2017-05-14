c2e74cbc49c7bd2a93f27cbfe0c91016254ea5bb
thatoshi <thatoshi@gmail.com>
2017-05-14 21:36:21 +0900

    Add GithubPractice by C++

---
+++ GithubPractice.cpp
@@ -0,0 +1,15 @@
+// GithubPractice.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+
+	std::cout << "GithubPractice" << std::endl;
+
+    return 0;
+}
+
